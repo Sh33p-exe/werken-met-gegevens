88f6a05423e5d73cfb4a23323b429fada737d9c7
Sh33p.exe <99063667@mydavinci.nl>
2021-11-09 09:45:12 +0100

    python calculations

---
+++ Feestlunch.py
@@ -0,0 +1,6 @@
+crossaint = 17 * 0.39
+stokbrood= 2 * 2.78
+Kortingsbon= 3 * 0.50
+prijs= (crossaint + stokbrood) - Kortingsbon 
+
+print(prijs)
---
+++ Speelhal.py
@@ -0,0 +1,7 @@
+Ticket= 7.45
+VRSeat= 9 * 0.37
+Personen= 4
+
+prijs= (VRSeat + Ticket) * Personen
+
+print(prijs)
